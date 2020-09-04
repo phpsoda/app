0c7ea53d6d108a51f281336b1a08e63e8ce7b709
Ibrokhim Shokirov <ibrokhim.shokirov@gmail.com>
2020-09-05 04:43:10 +0700

    Updated framework and enabled dependency injection

--- app/Controllers/ExampleController.php
+++ app/Controllers/ExampleController.php
@@ -2,15 +2,26 @@
 
 namespace App\Controllers;
 
+use App\Services\ExampleService;
 use PHPSoda\Http\JsonResponse;
 use PHPSoda\Http\Request;
 use PHPSoda\Http\Response;
 
 class ExampleController
 {
+    /**
+     * @var ExampleService
+     */
+    private $exampleService;
+
+    public function __construct(ExampleService $exampleService)
+    {
+        $this->exampleService = $exampleService;
+    }
+
     public function index(Request $request)
     {
-        return new Response('Get request to ExampleController@index.');
+        return new Response($this->exampleService->serve());
     }
     public function create(Request $request)
     {
